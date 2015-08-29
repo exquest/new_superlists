d544c05c406206bd363fffd35844a47391a32701
exquest <anthony.ainsworth@gmail.com>
2015-08-29 08:54:33 -0700

    added bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -104,6 +104,16 @@ class NewVisitorTest(LiveServerTestCase):
 		self.browser.set_window_size(1024, 768)
 		
 		# She notices the inputbox is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
+		
+		# She starts a new list and notices the input is nicely centered
+		# there too
+		inputbox.send_keys('testing\n')
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertAlmostEqual(
 			inputbox.location['x'] + inputbox.size['width'] / 2,
